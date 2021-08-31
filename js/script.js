bde580cb4bc90d3a62f1f689170afe3dc552f47e
Karina Klevtsova <88926633+kca2rie@users.noreply.github.com>
2021-08-31 11:44:38 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,11 @@
-start=1; finish=100;
-
-num=start-(start%2)+1;
-while(num<=finish) {
-  console.log(num);
-  num+=2;
+let n = 1000;
+for (let i = 2; i <= n; i++) {
+  for (let j = 2; j <= i; j++) {
+    if ((i % j == 0) && (j != i)) {
+      break;
+    } else {
+      console.log(i);
+      break;
+    }
+  }
 }
-
-
-
-const num1 = a;
-const num2 = b;
-const num3 = c;
-if(num1>num2 && num1>num3)
-console.log(num1);
-else if(num2>num1 && num2>num3) 
-console.log(num2);
-else if(num3>num1 && num3>num2)
-console.log(num3);
-else if(num1<num2 && num1<num3)
-console.log(num1);
-else if(num2<num1 && num2<num3)
-console.log(num2);
-else if(num3<num1 && num3<num2)
-console.log(num3);
-
-
-
-const rev = (str) => {
-    return str.split('')
-  };
-  
-  console.log(rev('hello, world!'));
