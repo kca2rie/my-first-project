1cf4cfac905df4f26a8aaa2e70ef6884de957392
Karina Klevtsova <88926633+kca2rie@users.noreply.github.com>
2021-09-08 13:27:50 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Задача 1
+
 function capitalize(str) {
   let x = "";
   for (i = 0; i < str.length; i++) {
@@ -9,3 +11,27 @@ function capitalize(str) {
   }
 return x;
 }
+
+
+// Задача 2
+
+const invertCase = (str) => { 
+  let index = 0;
+  let result = '';
+  let lowerStr = str.toLowerCase();
+  let upperStr = str.toUpperCase();
+  for (; index < str.length; index++) {
+    if (str[index] === lowerStr[index]) {
+      result +== upperStr[index];
+    }
+    else result +== lowerStr[index];
+  }
+  return result;
+}
+
+
+// Задача 3
+
+function isPowerOfThree() {
+  let n = "";
+  while (n % 3 === 0) {
