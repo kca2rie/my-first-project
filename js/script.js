cff44d0573e69c77b28e3ce775afe9e1bac90f37
Karina Klevtsova <88926633+kca2rie@users.noreply.github.com>
2021-09-19 16:47:34 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 // Задача 1
 
-
+const money = ['eur 10', 'usd 1', 'usd 10', 'rub 50', 'usd 5'];
+const getTotalAmount = (money, currency) => {
+  let summ = 0;
+  for(let i = 0; i < money.length; i++) {
+    const bill = money[i].split(' ');
+    if(currency === bill[0]) {
+      summ += +bill[1]; 
+    }
+  }
+  return summ;
+}
+console.log(getTotalAmount(money, 'usd'))
+console.log(getTotalAmount(money, 'rub'))
+console.log(getTotalAmount(money, 'eur'))
 
 
 // Задача 2
